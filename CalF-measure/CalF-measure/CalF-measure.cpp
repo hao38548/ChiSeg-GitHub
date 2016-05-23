d764f42300e7e029787c04ee2d07f4b06f5b0ddb
hao38548 <937592093@qq.com>
2016-05-23 16:54:22 +0800

    计算F值,调试通过

--- CalF-measure/CalF-measure/CalF-measure.cpp
+++ CalF-measure/CalF-measure/CalF-measure.cpp
@@ -3,58 +3,75 @@
 
 #include<iostream>
 #include<string>
+#include <fstream>
 
 using namespace std;
+typedef unsigned long long  UINT;
 
-
-void main()
+float CalF_measure(UINT& TP, UINT& FN, UINT& FP, string right_file, string real_file, string result_file)
 {
 	int i = 0;
 	string right_code, real_code;
-	float acc_rate = 0.0f;
+	float F_measure = 0.0f;
 	ifstream in1;
 	ifstream in2;
 	ofstream out;
-	in1.open("E:\\coding\\ChineseSegment\\标记语料信息\\5000标记\\农业\\拆分\\编码信息.txt", ios::in);
-	//in2.open("D:\\coding\\ChineseSegment\\标记语料信息\\5000标记\\电子\\拆分\\ICTCLAS切分编码.txt",ios::in);
-	in2.open("E:\\coding\\ChineseSegment\\测试文件\\MI-marked-seg-result-codes.txt", ios::in);
-	out.open("E:\\coding\\ChineseSegment\\测试文件\\MI-marked-seg-accuracy.txt", ios::out);
+	in1.open(right_file, ios::in);
+	in2.open(real_file, ios::in);
+	out.open(result_file, ios::out);
 	while (getline(in1, right_code))
 	{
 		getline(in2, real_code);
-		acc_rate = CalAccuracyRate(real_code, right_code);
-		out << acc_rate << endl;
-		i++;
-		if (i % 1000 == 0)
-			//if (i == 37)
+		int code_len = real_code.length();
+		for (int i = 0; i < code_len; i++)
 		{
-			cout << ".";
-		}
-		if (i == 1815)
-		{
-			cout << ".";
+			switch (right_code.at(i))
+			{
+			case '1':
+				if (real_code.at(i) == '1')
+				{
+					//正类分为了正类
+					TP++;
+				}
+				else if (real_code.at(i) == '0')
+				{
+					//应该为正类分为负类的数目
+					FN++;
+				}
+				break;
+			case '0':
+				if (real_code.at(i) == '1')
+				{
+					//应该为0，结果分为了1
+					FP++;
+				}
+				break;
+			case '2':
+				//可以切分也可以不切分的认为切分正确
+				TP++;
+				break;
+			}
 		}
 	}
 	in1.close();
 	in2.close();
 	out.close();
-
+	//计算F_measure
+	F_measure = 2.000 * TP / (2 * TP + FP + FN);
+	return F_measure;
 }
 
-//计算准确率
-float CalAccuracyRate(string real_code, string right_code)
+void main()
 {
-	//编码长度
-	int code_len = real_code.length();
-	int right_cnt = 0;
-	for (int i = 0; i < code_len; i++)
-	{
-		if (real_code[i] == right_code[i] || right_code[i] == '2')
-		{
-			//该位编码匹配成功/该位为2，即为既可以拆分也可以不拆分
-			right_cnt++;
-		}
-	}
-	return (float)right_cnt / code_len;
+	UINT TP = 0;
+	UINT FN = 0;
+	UINT FP = 0;
+	string right_file = "E:\\coding\\ChineseSegment\\标记语料信息\\5000标记\\农业\\拆分\\编码信息.txt";
+	string real_file = "E:\\coding\\ChineseSegment\\测试文件\\MI-marked-seg-result-codes.txt";
+	string result_file = "E:\\coding\\ChineseSegment\\测试文件\\MI-marked-seg-accuracy.txt";
+	float F_measure = CalF_measure(TP, FN, FP,right_file, real_file, result_file);
+	cout << "F值为：" << F_measure;
 }
+
+
 
